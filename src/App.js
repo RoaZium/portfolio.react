169a326e3f7c9e1cad8e3228507f9779d9e58072
황진욱 <roazium@gmail.com>
2022-04-01 10:43:27 +0900

    Proxy 작업

--- src/App.js
+++ src/App.js
@@ -1,3 +1,71 @@
+import './App.css';
+import axios from 'axios';
+import React,{useState,useEffect} from 'react';
+
+function App() {
+
+  const sendRequest = async() => {
+    const response = await axios.get('http://localhost:5000');
+    console.log(response);
+    console.log(response.data);
+  };
+
+  useEffect(()=>{
+    sendRequest();    
+  });
+
+  return (
+    <div className="App">      
+    </div>
+  );
+}
+
+export default App;
+
+/* import axios from "axios";
+import { useEffect } from 'react';
+
+function App() {
+  const callApi = async()=>{
+    axios.get("/api").then((res)=>{console.log(res.data.test)});
+  };
+
+  useEffect(()=>{
+    callApi();
+  }, []);
+  
+  return (
+    <div className="App">
+	...
+    </div>
+  );
+}
+
+export default App; */
+
+/* import axios from "axios";
+import { useEffect } from 'react';
+
+function App() {
+  const callApi = async()=>{
+    axios.get("/api").then((res)=>{console.log(res.data.test)});
+  };
+
+  useEffect(()=>{
+    callApi();
+  }, []);
+  
+  return (
+    <div className="App">
+	...
+    </div>
+  );
+}
+
+export default App; */
+
+/* export default App;
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -22,4 +90,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; */
---
+++ src/setupProxy.js
@@ -0,0 +1,22 @@
+// setupProxy.js
+const {createProxyMiddleware} = require('http-proxy-middleware');
+
+module.exports = function(app){
+  app.use(
+    createProxyMiddleware("/api",{
+      target:"http://localhost:5000",
+      changeOrigin:true,
+    })
+  )
+}
+
+/* const proxy = require('http-proxy-middleware');
+
+module.exports = (app) => {
+  app.use(
+    proxy('/api', {  //도메인 api로 호출
+      target: 'http://localhost:5000/', //통신할 서버의 도메인주소
+      changeOrigin: true,
+    })
+  )
+} */
