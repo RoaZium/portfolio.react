cf4bd4059fd954b34afc212b34e8df11911a2301
황진욱 <40532902+RoaZium@users.noreply.github.com>
2022-04-27 11:00:12 +0900

    [개발] index.js 코드 정리

--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import StateHook from "./examples/useStateHook";
-import Appbar from "./components/Appbar";
-
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Main from "./layouts/Main";
-import PropsExample from "./examples/props";
 
 const rootElement = document.getElementById("root");
 
@@ -18,21 +12,5 @@ ReactDOM.render(
       <App />
     </div>
   </BrowserRouter>,
-  document.getElementById("root")
+  rootElement
 );
-
-//props example.01
-/* ReactDOM.render(
-  <>
-    <PropsExample title="222" body="health" />
-  </>,
-  rootElement
-); */
-
-// props example.02
-/* ReactDOM.render(
-  <>
-    <PropsExample name="HJW" color="blue" brand="Rapido" />
-  </>,
-  rootElement
-); */
