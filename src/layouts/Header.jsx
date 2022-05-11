bf2489853473f05cb985ddbb61b5ce7ad73d58be
황진욱 <40532902+RoaZium@users.noreply.github.com>
2022-05-11 09:28:07 +0900

    Trash File 정리

--- src/layouts/Header.jsx
+++
@@ -1,5 +0,0 @@
-const Header = () => {
-  return <h2>This is Footer</h2>;
-};
-
-export default Header;
---
+++ src/layouts/admin/Main.jsx
@@ -0,0 +1,166 @@
+import * as React from "react";
+import { styled, useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import MuiDrawer from "@mui/material/Drawer";
+import MuiAppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import CssBaseline from "@mui/material/CssBaseline";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import { Link, Route, Routes } from "react-router-dom";
+import Visitor01 from "./visitors/Visitor01";
+import Visitor02 from "./visitors/Visitor02";
+import Visitor03 from "./visitors/Visitor03";
+import Visitor04 from "./visitors/Visitor04";
+import Visitor05 from "./visitors/Visitor05";
+import { Button } from "@mui/material";
+
+const drawerWidth = 240;
+
+const openedMixin = (theme) => ({
+  width: drawerWidth,
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.enteringScreen,
+  }),
+  overflowX: "hidden",
+});
+
+const closedMixin = (theme) => ({
+  transition: theme.transitions.create("width", {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  overflowX: "hidden",
+  width: `calc(${theme.spacing(7)} + 1px)`,
+  [theme.breakpoints.up("sm")]: {
+    width: `calc(${theme.spacing(8)} + 1px)`,
+  },
+});
+
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  zIndex: theme.zIndex.drawer + 1,
+  transition: theme.transitions.create(["width", "margin"], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    marginLeft: drawerWidth,
+    width: `calc(100% - ${drawerWidth}px)`,
+    transition: theme.transitions.create(["width", "margin"], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  width: drawerWidth,
+  flexShrink: 0,
+  whiteSpace: "nowrap",
+  boxSizing: "border-box",
+  ...(open && {
+    ...openedMixin(theme),
+    "& .MuiDrawer-paper": openedMixin(theme),
+  }),
+  ...(!open && {
+    ...closedMixin(theme),
+    "& .MuiDrawer-paper": closedMixin(theme),
+  }),
+}));
+
+export default function MiniDrawer() {
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    // <BrowserRouter>
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed" open={open}>
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{
+              marginRight: 2,
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div">
+            방문자
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open} >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === "rtl" ? (
+              <ChevronRightIcon />
+            ) : (
+              <ChevronLeftIcon />
+            )}
+          </IconButton>
+        </DrawerHeader>
+        <Link to="/" style={{ textDecoration: `none` }}>
+          <Button >홈</Button>
+        </Link>
+        <Link to="/Visitor01" style={{ textDecoration: `none` }}>
+          <Button>방문01</Button>
+        </Link>
+        <Link to="/Visitor02" style={{ textDecoration: `none` }}>
+          <Button>방문02</Button>
+        </Link>
+        <Link to="/Visitor03" style={{ textDecoration: `none` }}>
+          <Button>방문03</Button>
+        </Link>
+        <Link to="/Visitor04" style={{ textDecoration: `none` }}>
+          <Button>방문04</Button>
+        </Link>
+        <Link to="/Visitor05" style={{ textDecoration: `none` }}>
+          <Button>방문05</Button>
+        </Link>
+      </Drawer>
+      <Box component="main" sx={{ flexGrow: 1, p: 0 }}>
+        <DrawerHeader />
+        <Routes>
+          <Route path="/" element={<Visitor01 />} />
+          <Route path="/Visitor01" element={<Visitor01 />} />
+          <Route path="/Visitor02" element={<Visitor02 />} />
+          <Route path="/Visitor03" element={<Visitor03 />} />
+          <Route path="/Visitor04" element={<Visitor04 />} />
+          <Route path="/Visitor05" element={<Visitor05 />} />
+        </Routes>
+      </Box>
+    </Box>
+    // </BrowserRouter>
+  );
+}
