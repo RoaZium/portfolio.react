2d8257dd75bb303b5c827dc06608c4f7b5c6c08b
황진욱 <40532902+RoaZium@users.noreply.github.com>
2022-05-13 17:19:37 +0900

    코드 정리

--- src/layouts/visitors/Main.jsx
+++ src/layouts/visitors/Main.jsx
@@ -1,93 +1,16 @@
 import * as React from "react";
-import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
 import CssBaseline from "@mui/material/CssBaseline";
 import Typography from "@mui/material/Typography";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import { Link, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Visitor01 from "./Visitor01";
 import Visitor02 from "./Visitor02";
 import Visitor03 from "./Visitor03";
 import Visitor04 from "./Visitor04";
 import Visitor05 from "./Visitor05";
-import { Button, Card, Container } from "@mui/material";
+import { Container } from "@mui/material";
 import CI from "../../assets/images/CI.png";
 import Grid from "@mui/material/Grid";
-import Stack from "@mui/material/Stack";
-import { height } from "@mui/material/node_modules/@mui/system";
-
-const drawerWidth = 240;
-
-const openedMixin = (theme) => ({
-  width: drawerWidth,
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
-  }),
-  overflowX: "hidden",
-});
-
-const closedMixin = (theme) => ({
-  transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  overflowX: "hidden",
-  width: `calc(${theme.spacing(7)} + 1px)`,
-  [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
-  },
-});
-
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  zIndex: theme.zIndex.drawer + 1,
-  transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(["width", "margin"], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-const Drawer = styled(MuiDrawer, {
-  shouldForwardProp: (prop) => prop !== "open",
-})(({ theme, open }) => ({
-  width: drawerWidth,
-  flexShrink: 0,
-  whiteSpace: "nowrap",
-  boxSizing: "border-box",
-  ...(open && {
-    ...openedMixin(theme),
-    "& .MuiDrawer-paper": openedMixin(theme),
-  }),
-  ...(!open && {
-    ...closedMixin(theme),
-    "& .MuiDrawer-paper": closedMixin(theme),
-  }),
-}));
 
 export default function Main() {
   return (
--- src/layouts/visitors/Visitor01.jsx
+++ src/layouts/visitors/Visitor01.jsx
@@ -12,9 +12,11 @@ import PersonSearchIcon from "@mui/icons-material/PersonSearch";
 const buttonStyle = {
   minWidth: "300px",
   minHeight: "300px",
+  backgroundColor: "#62b4f4",
   border: "3px solid",
   borderColor: "yellow",
   borderRadius: 5,
+  boxShadow: 13,
 };
 
 export default function Visitor01() {
@@ -22,9 +24,7 @@ export default function Visitor01() {
     <Box
       display="flex"
       alignItems="center"
-      justifyContent="center"
       sx={{
-        bgcolor: "red",
         height: "80vh",
       }}
     >
@@ -35,68 +35,53 @@ export default function Visitor01() {
           container
           justifyContent="center"
           alignContent="center"
-          bgcolor="purple"
         >
-          <Box
-            bgcolor="transparent"
-            color="info.contrastText"
-            sx={{
-              boxShadow: 13,
-              borderRadius: 5,
-            }}
-          >
-            <Link to="/Visitor02" style={{ textDecoration: "none" }}>
-              <Button bgcolor="red" sx={buttonStyle} variant="outlined">
-                방문자 예약 신청
-                <AssignmentIndIcon
-                  sx={{
-                    height: "100px",
-                    width: "100px",
-                    position: "absolute",
-                    top: "30px",
-                  }}
-                />
-              </Button>
-            </Link>
-          </Box>
+          <Link to="/Visitor02" style={{ textDecoration: "none" }}>
+            <Button bgcolor="red" sx={buttonStyle} variant="outlined">
+              방문자 예약 신청
+              <AssignmentIndIcon
+                sx={{
+                  height: "100px",
+                  width: "100px",
+                  position: "absolute",
+                  top: "30px",
+                  color: "yellow",
+                }}
+              />
+            </Button>
+          </Link>
         </Grid>
         <Grid item xs={7} container>
-          <Grid
-            item
-            xs={12}
-            height="45%"
-            bgcolor="orange"
-            container
-            justifyContent="center"
-          >
+          <Grid item xs={12} height="45%" container justifyContent="center">
             <Button
               sx={[
                 {
                   boxShadow: 13,
                   minWidth: "470px",
                   border: "3px solid",
-                  color: "blue",
-                  bgcolor: "black",
+                  bgcolor: "#62b4f4",
                   borderColor: "yellow",
                   borderRadius: 5,
                 },
               ]}
               variant="outlined"
-              startIcon={<PersonSearchIcon />}
+              startIcon={<PersonSearchIcon sx={{ color: "yellow" }} />}
             >
               방문자 예약 조회
             </Button>
           </Grid>
-          <Grid item xs={12} height="10%" bgcolor="yellowgreen"></Grid>
           <Grid
             item
-            height="45%"
             xs={12}
-            container
-            justifyContent="center"
-            bgcolor="blue"
-          >
+            height="10%"
+            sx={{
+              pointerEvents: "none",
+            }}
+          ></Grid>
+          <Grid item height="45%" xs={12} container justifyContent="center">
             <Card
+              justifyContent="center"
+              alignContent="center"
               sx={{
                 width: "470px",
                 border: "3px solid",
@@ -104,11 +89,23 @@ export default function Visitor01() {
                 borderColor: "yellow",
                 boxShadow: 13,
                 borderRadius: 5,
-                bgcolor: "red",
+                bgcolor: "white",
               }}
             >
-              <Typography>① 방문절차 안내</Typography>
-              <Typography>
+              <Typography
+                sx={{
+                  marginLeft: "30px",
+                  color: "black",
+                }}
+              >
+                ① 방문절차 안내
+              </Typography>
+              <Typography
+                textAlign="center"
+                sx={{
+                  color: "black",
+                }}
+              >
                 방문신청 &gt; 피방문자 통보 &gt; 내부승인 &gt; 방문수속 &gt;
                 방문
               </Typography>
