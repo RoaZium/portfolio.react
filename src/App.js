583075714cb226207a8318406026de1b0cba6f9f
황진욱 <40532902+RoaZium@users.noreply.github.com>
2022-05-20 15:00:12 +0900

    [개발] app, index 정리

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import Visitor from "./layouts/visitors/Main";
 
 const App = () => {
-  return (
-    <div>
-      <Visitor />
-    </div>
-  );
+  return <Visitor />;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Visitor from "./layouts/visitors/Main";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./assets/theme";
+import App from "./App";
 
 const rootElement = document.getElementById("root");
 
 ReactDOM.render(
   <BrowserRouter>
     <ThemeProvider theme={theme}>
-      <Visitor />
+      <App />
     </ThemeProvider>
   </BrowserRouter>,
   rootElement
