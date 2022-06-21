72161b0672ce70140c1200e112634aeea6038c38
황진욱 <40532902+RoaZium@users.noreply.github.com>
2022-06-21 18:01:23 +0900

    [개발] 로그인 암호화 기능 추가

--- src/layouts/admins/Login.jsx
+++ src/layouts/admins/Login.jsx
@@ -8,11 +8,72 @@ import {
   Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Logo from "../../assets/images/logo.png";
 import Company from "../../assets/images/Login_company.jpg";
+import { useState } from "react";
+import { sha512 } from "js-sha512";
+import axios from "axios";
 
 export default function Login() {
+  const navigate = useNavigate();
+  const [id, setID] = useState();
+  const [password, setPassword] = useState();
+  const [encryptionPassword, setEncryptionPassword] = useState();
+
+  const LoginUser = () => {
+    if (id === undefined || id === null) {
+      alert("ID를 입력하세요.");
+      return;
+    }
+
+    if (password === undefined || password === null) {
+      alert("패스워드를 입력하세요.");
+      return;
+    }
+
+    setEncryptionPassword(sha512(password));
+    PostLogin();
+  };
+
+  var data = JSON.stringify({
+    id: id,
+    password: encryptionPassword,
+  });
+
+  var config = {
+    method: "post",
+    url: "/login",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    data: data,
+  };
+
+  const PostLogin = async () => {
+    axios(config)
+      .then(function (response) {
+        console.log(JSON.stringify(response.data["code"]));
+        console.log(data);
+        var code = JSON.stringify(response.data["code"]);
+
+        if (code !== "1") {
+          alert("비밀번호가 틀립니다.");
+          return;
+        }
+
+        navigate("/VisitorManagement");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
+  const handleChange = (prop) => (event) => {
+    setPassword(event.target.value);
+    setEncryptionPassword(sha512(event.target.value));
+  };
+
   return (
     <Box
       display="flex"
@@ -80,6 +141,8 @@ export default function Login() {
                     <AccountCircle />
                   </InputAdornment>
                 }
+                value={id}
+                onChange={(event) => setID(event.target.value)}
                 sx={{
                   borderRadius: 10,
                   height: "50px",
@@ -105,6 +168,9 @@ export default function Login() {
                     <Lock />
                   </InputAdornment>
                 }
+                type="password"
+                value={password}
+                onChange={handleChange("password")}
                 sx={{
                   borderRadius: 10,
                   height: "50px",
@@ -123,19 +189,19 @@ export default function Login() {
             justifyContent="center"
           >
             <Box>
-              <Link to="/VisitorManagement" style={{ textDecoration: "none" }}>
-                <Button
-                  variant="contained"
-                  sx={{
-                    borderRadius: 10,
-                    alignSelf: "center",
-                    justifySelf: "center",
-                    width: "350px",
-                  }}
-                >
-                  SIGN IN
-                </Button>
-              </Link>
+              <Button
+                variant="contained"
+                sx={{
+                  borderRadius: 10,
+                  alignSelf: "center",
+                  justifySelf: "center",
+                  height: "50px",
+                  width: "350px",
+                }}
+                onClick={LoginUser}
+              >
+                로그인
+              </Button>
             </Box>
           </Grid>
         </Grid>
